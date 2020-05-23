542ea6bf1d7f8cc778d166d44397ba2ccaff3232
Sergio Hideki Ono <sergiohidekiono@gmail.com>
2020-05-23 15:16:31 -0300

    Component grid de produto

--- shop/lib/views/products_overview_screen.dart
+++ shop/lib/views/products_overview_screen.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
+import '../data/dummy_data.dart';
+import '../models/product.dart';
 
 class ProductOverViewScreen extends StatelessWidget {
+  final List<Product> loadedProducts = DUMMY_PRODUCTS;
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Minha Loja'),
+      ),
+      body: GridView.builder(
+        padding: const EdgeInsets.all(10),
+        itemCount: loadedProducts.length,
+        itemBuilder: (ctx, index) => Text(loadedProducts[index].title),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+        ),
+      ),
     );
   }
 }
