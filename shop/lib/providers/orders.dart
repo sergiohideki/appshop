86c6dd94721d4a620285d90dce11865b6ef0ee62
Sergio Hideki Ono <sergiohidekiono@gmail.com>
2020-05-26 19:51:54 -0300

    componente pedidos #2

--- shop/lib/providers/orders.dart
+++ shop/lib/providers/orders.dart
@@ -19,14 +19,18 @@ class Order {
 }
 
 class Orders with ChangeNotifier {
-  List<Order> _orders = [];
+  List<Order> _items = [];
 
-  List<Order> get orders {
-    return [...orders];
+  List<Order> get items {
+    return [..._items];
+  }
+
+  int get itemsCount {
+    return _items.length;
   }
 
   void addOrder(Cart cart) {
-    _orders.insert(
+    _items.insert(
       0,
       Order(
         id: Random().nextDouble().toString(),
