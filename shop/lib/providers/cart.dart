ebdef99dbb7fd4e3c2d04da9e264a344a96286b8
Sergio Hideki Ono <sergiohidekiono@gmail.com>
2020-05-26 18:40:19 -0300

    remove item #1

--- shop/lib/providers/cart.dart
+++ shop/lib/providers/cart.dart
@@ -5,12 +5,14 @@ import './product.dart';
 
 class CartItem {
   final String id;
+  final String productId;
   final String title;
   final int quantity;
   final double price;
 
   CartItem({
     @required this.id,
+    @required this.productId,
     @required this.title,
     @required this.quantity,
     @required this.price,
@@ -43,6 +45,7 @@ class Cart with ChangeNotifier {
         product.id,
         (existingItem) => CartItem(
           id: existingItem.id,
+          productId: existingItem.productId,
           title: existingItem.title,
           quantity: existingItem.quantity + 1,
           price: existingItem.price,
@@ -53,6 +56,7 @@ class Cart with ChangeNotifier {
         product.id,
         () => CartItem(
           id: Random().nextDouble().toString(),
+          productId: product.id,
           title: product.title,
           quantity: 1,
           price: product.price,
@@ -60,6 +64,11 @@ class Cart with ChangeNotifier {
       );
     }
 
+    notifyListeners();
+  }
+
+  void removeItem(String productId){
+    _items.remove(productId);
     notifyListeners();
   }
 }
