e6bdb02115e1e1ad16957d99ace1565b1fc6ed2b
Sergio Hideki Ono <sergiohidekiono@gmail.com>
2020-05-26 13:14:22 -0300

    Multiprovider

--- shop/lib/main.dart
+++ shop/lib/main.dart
@@ -4,15 +4,24 @@ import 'package:provider/provider.dart';
 import './views/products_overview_screen.dart';
 import './utils/app_routes.dart';
 import './views/product_detail_screen.dart';
+
 import './providers/products.dart';
+import './providers/cart.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (_) => Products(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => Products(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => Cart(),
+        ),
+      ],
       child: MaterialApp(
         theme: ThemeData(
           primarySwatch: Colors.purple,
