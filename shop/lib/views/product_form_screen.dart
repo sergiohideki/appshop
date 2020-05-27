9a39f927cc12bc4d04149cf7f9f2d3686446c288
Sergio Hideki Ono <sergiohidekiono@gmail.com>
2020-05-26 22:48:26 -0300

    Usando o form do flutter

--- shop/lib/views/product_form_screen.dart
+++ shop/lib/views/product_form_screen.dart
@@ -12,6 +12,19 @@ class _ProductFormScreenState extends State<ProductFormScreen> {
       appBar: AppBar(
         title: Text('Formulário Produto'),
       ),
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Form(
+          child: ListView(
+            children: <Widget>[
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Título'),
+                textInputAction: TextInputAction.next,
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
