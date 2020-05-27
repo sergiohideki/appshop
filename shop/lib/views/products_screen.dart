00e8fad3eb850fecf6101492594f4e1a717174ba
Sergio Hideki Ono <sergiohidekiono@gmail.com>
2020-05-26 22:39:31 -0300

    componente gerenciamento de produto #2

--- shop/lib/views/products_screen.dart
+++ shop/lib/views/products_screen.dart
@@ -3,12 +3,13 @@ import 'package:provider/provider.dart';
 
 import '../widgets/app_drawer.dart';
 import '../providers/products.dart';
+import '../widgets/product_item.dart';
 
 class ProductsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final products = Provider.of<Products>(context);
-
+    final productsData = Provider.of<Products>(context);
+    final products = productsData.items;
     return Scaffold(
       appBar: AppBar(
         title: Text('Gerenciar Produtos'),
@@ -23,8 +24,13 @@ class ProductsScreen extends StatelessWidget {
       body: Padding(
         padding: EdgeInsets.all(8),
         child: ListView.builder(
-          itemCount: products.itemsCount,
-          itemBuilder: (ctx, i) => Text('teste'),
+          itemCount: productsData.itemsCount,
+          itemBuilder: (ctx, i) => Column(
+            children: <Widget>[
+              ProductItem(products[i]),
+              Divider(),
+            ],
+          ),
         ),
       ),
     );
