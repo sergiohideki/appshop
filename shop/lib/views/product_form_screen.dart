62e68d9ca9e0a697e64701055fdfd759055824bc
Sergio Hideki Ono <sergiohidekiono@gmail.com>
2020-05-27 12:38:09 -0300

    validação do formulário #2

--- shop/lib/views/product_form_screen.dart
+++ shop/lib/views/product_form_screen.dart
@@ -24,7 +24,20 @@ class _ProductFormScreenState extends State<ProductFormScreen> {
   }
 
   void _updateImageUrl() {
-    setState(() {});
+    if (isValidImageUrl(_imageUrlController.text)) {
+      setState(() {});
+    }
+  }
+
+  bool isValidImageUrl(String url) {
+    bool startWithHttp = url.toLowerCase().startsWith('http://');
+    bool startWithHttps = url.toLowerCase().startsWith('https://');
+    bool endsWithPng = url.toLowerCase().endsWith('.png');
+    bool endsWithJpg = url.toLowerCase().endsWith('.jpg');
+    bool endsWithJpeg = url.toLowerCase().endsWith('.jpeg');
+    return (startWithHttp || startWithHttps) && (endsWithPng) ||
+        (endsWithJpg) ||
+        (endsWithJpeg);
   }
 
   @override
@@ -79,12 +92,11 @@ class _ProductFormScreenState extends State<ProductFormScreen> {
                 },
                 onSaved: (value) => _formData['title'] = value,
                 validator: (value) {
-                  if(value.trim().isEmpty){
-                  return 'Informe um titulo valido!';
-                  }
+                  bool isEmpty = value.trim().isEmpty;
+                  bool isInvalid = value.trim().length < 3;
 
-                  if(value.trim().length <= 3){
-                  return 'Informe um titulo com no mínimo 3 letras!';
+                  if (isEmpty || isInvalid) {
+                    return 'Informe um titulo válido com no mínimo 3 caracteres!';
                   }
                   return null;
                 },
@@ -98,6 +110,17 @@ class _ProductFormScreenState extends State<ProductFormScreen> {
                   FocusScope.of(context).requestFocus(_descriptionFocusNode);
                 },
                 onSaved: (value) => _formData['price'] = double.parse(value),
+                validator: (value) {
+                  bool isEmpty = value.trim().isEmpty;
+                  var newPrice = double.tryParse(value);
+                  bool isInvalid = newPrice == null || newPrice <= 0;
+
+                  if (isEmpty || isInvalid) {
+                    return 'Informe um preço válido!';
+                  }
+
+                  return null;
+                },
               ),
               TextFormField(
                 decoration: InputDecoration(labelText: 'Descrição'),
@@ -105,6 +128,16 @@ class _ProductFormScreenState extends State<ProductFormScreen> {
                 keyboardType: TextInputType.multiline,
                 focusNode: _descriptionFocusNode,
                 onSaved: (value) => _formData['description'] = value,
+                validator: (value) {
+                  bool isEmpty = value.trim().isEmpty;
+                  bool isInvalid = value.trim().length < 10;
+
+                  if (isEmpty || isInvalid) {
+                    return 'Informe um descrição válido com no mínimo 10 caracteres!';
+                  }
+
+                  return null;
+                },
               ),
               Row(
                 crossAxisAlignment: CrossAxisAlignment.end,
@@ -120,6 +153,14 @@ class _ProductFormScreenState extends State<ProductFormScreen> {
                         _saveForm();
                       },
                       onSaved: (value) => _formData['imageUrl'] = value,
+                      validator: (value) {
+                        bool isEmpty = value.trim().isEmpty;
+                        bool isInvalid = !isValidImageUrl(value);
+
+                        if (isEmpty || isInvalid) {
+                          return 'Informe uma URL válida!';
+                        }
+                      },
                     ),
                   ),
                   Container(
